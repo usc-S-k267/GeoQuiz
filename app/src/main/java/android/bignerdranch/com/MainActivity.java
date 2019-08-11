f5468f12d13f50947a26bc3375e0cf41f9c85051
taha1 <s_k267@student.usc.edu.au>
2019-08-11 15:49:41 +0200

    Week 2 changes

--- app/src/main/java/android/bignerdranch/com/MainActivity.java
+++ app/src/main/java/android/bignerdranch/com/MainActivity.java
@@ -1,62 +1,59 @@
+
 package android.bignerdranch.com;
-import android.support.V7.app.AppCompatactivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
-    private Button mFalsebutton;
+    private Button mFalseButton;
+    private int mCurrentIndex;
+    private int SavedInstanceState;
+    private Button mCheatButton;
+    private TextView mQuestionTextView;
 
 
-@Override
-protected void onCreate (Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    mTrueButton = (Button) findViewById(R.id.true_button);
-    mFalseButton = (Button)findViewById(R.id.false_button);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState !=null) {
+            mCurrentIndex = savedInstanceState.getInt("index", 0);
+        }
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onSaveInstanceState (Bundle savedInstanceState) {
+        super.onSaveInstanceState (savedInstanceState);
+        savedInstanceState.putInt("index", mCurrentIndex);
     }
 }
 
-mTrueButton = (Button) findViewById (R.id. true_button);
-mTrueButton.setOnClicklistener(new View.OnClickListener()
-{
-    @Override
-    public void onClick(View v) {
-
-    }
-});
-mFalsteButton = (Button) findViewById(R.id. false_button);
-mFalseButton.setOnClicklistener(new View.OnClickListener)()
-{
-    @Override
-    public void onClick (view v) {
-
-    }
-});
-
-@Override
-public void onSaveInstanceState (Bundle savedInstanceState) {
-    super onSaveInstanceState (savedInstanceState);
-        savedInstanceState.putInt("index", mCurrentIndex);
-
-        }
-
-
-
-mTrueButton.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View v) {
-        Toast.makeText(QuizActivity.this,
-        R.string.incorrect_toast,
-        Toast.LENGTH_SHORT).show();
-        }
-        });
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View v) {
-        Toast.makeText(QuizActivity.this,
-        R.string.correct_toast,
-        Toast.LENGTH_SHORT).show();
-        }
-        });
