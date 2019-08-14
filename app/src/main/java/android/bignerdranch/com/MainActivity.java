aa6b700138083bbc396707ab0f1f538a6218fd4f
taha1 <s_k267@student.usc.edu.au>
2019-08-14 04:56:42 +0200

    Added new buttons fixed up code

--- app/src/main/java/android/bignerdranch/com/MainActivity.java
+++ app/src/main/java/android/bignerdranch/com/MainActivity.java
@@ -1,59 +1,139 @@
 
 package android.bignerdranch.com;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private int mCurrentIndex;
-    private int SavedInstanceState;
-    private Button mCheatButton;
+    private Button mNextButton;
     private TextView mQuestionTextView;
+    private Button mCheatButton;
+    private static final String TAG = "QuizActivity";
 
+
+            private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_oceans, true, false),
+            new TrueFalse(R.string.question_mideast, false, false),
+            new TrueFalse(R.string.question_africa, false, false),
+            new TrueFalse(R.string.question_americas, true, false),
+            new TrueFalse(R.string.question_asia, true, false)
+    };
+    private boolean[] mCheaterStatus = new boolean[mQuestionBank.length];
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+}
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show();
+    }
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        mTrueButton = (Button)findViewById(R.id.true_button);
         setContentView(R.layout.activity_main);
+
 
         if (savedInstanceState !=null) {
             mCurrentIndex = savedInstanceState.getInt("index", 0);
         }
-
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        mFalseButton = (Button) findViewById(R.id.false_button);
-
-
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
-
-            }
-        });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Toast.makeText(MainActivity.this,
                         R.string.correct_toast,
                         Toast.LENGTH_SHORT).show();
+
+                mTrueButton = (Button) findViewById(R.id.true_button);
+                mFalseButton = (Button) findViewById(R.id.false_button);
+
+                mNextButton = (Button)findViewById(R.id.next_button);
+                mNextButton.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                        updateQuestion();
+
+
+
+
+                        mTrueButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this,
+                                R.string.incorrect_toast,
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                mFalseButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this,
+                                R.string.correct_toast,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
             }
+
+            @Override
+            public void onSaveInstanceState(Bundle savedInstanceState) {
+                super.onSaveInstanceState(savedInstanceState);
+                savedInstanceState.putInt("index", mCurrentIndex);
+            }
+
+
         });
-
     }
-
-    @Override
-    public void onSaveInstanceState (Bundle savedInstanceState) {
-        super.onSaveInstanceState (savedInstanceState);
-        savedInstanceState.putInt("index", mCurrentIndex);
     }
 }
+}
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+        checkAnswer(true);
+        }
+        });
+
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        }
+        });
+        mNextButton = (Button)findViewById(R.id.next_button);
+        }
+        }
+}
--- app/src/main/java/android/bignerdranch/com/TrueFalse.java
+++ app/src/main/java/android/bignerdranch/com/TrueFalse.java
@@ -1,21 +1,28 @@
+package android.bignerdranch.com;
 public class TrueFalse {
+
     private int mQuestion;
     private boolean mTrueQuestion;
 
-    public TrueFalse(int question, boolean trueQuestion) {
+    public TrueFalse(int question, boolean trueQuestion, boolean isCheater){
         mQuestion = question;
         mTrueQuestion = trueQuestion;
     }
+
     public int getQuestion() {
         return mQuestion;
     }
+
     public void setQuestion(int question) {
         mQuestion = question;
     }
+
     public boolean isTrueQuestion() {
         return mTrueQuestion;
     }
+
     public void setTrueQuestion(boolean trueQuestion) {
         mTrueQuestion = trueQuestion;
     }
+
 }
